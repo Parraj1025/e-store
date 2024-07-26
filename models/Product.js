a58b7be43c25e18fc7327bfda044b91bb919d1c1
Juan Parra <parrajuan1025@gmail.com>
2024-07-26 15:02:17 -0400

    got tables structured and relationships set

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -15,7 +16,7 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-    product_mame: {
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
@@ -30,6 +31,10 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id'
+      }
     }
     // define columns
   },
